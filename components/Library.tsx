d755d7d76802948e66f555306e7d7b654c3a109f
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2023-07-07 00:04:21 +0900

    Done up to "Songs fetching and list display"

--- components/Library.tsx
+++ components/Library.tsx
@@ -5,8 +5,16 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({
+  songs
+}) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -34,7 +42,13 @@ const Library = () => {
         />
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
-        List of Songs!
+        {songs.map((item) => (
+          <MediaItem
+            onClick={() => {}}
+            key={item.id}
+            data={item}
+          />
+        ))}
       </div>
     </div>
   )
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "ticcadbaqmqifdgpoatk.supabase.co"
+    ]
+  }
+}
 
 module.exports = nextConfig
